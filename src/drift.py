a886de488baae2f2fd93cc5276da79a0b9532fbb
Paulo Nuin <nuin@genedrift.org>
2021-01-25 20:41:19 -0700

    saving json file

--- src/drift.py
+++ src/drift.py
@@ -25,10 +25,10 @@ def calculate_frequency(population):
 
 
 @click.command()
-@click.option('-p', '--pop-size', 'pop_size', default=10, help='Population size')
+@click.option('-p', '--pop-size', 'pop_size', default=100, help='Population size')
 @click.option('-f', '--freq-A', 'freq_A', default=0.5, help='Initial A frequency')
 @click.option('-n', '--n-generations', 'number_generations', default=100, help='Number of generations')
-@click.option('-np','--n-populations', 'number_populations', default=2, help='Number of populations')
+@click.option('-np','--n-populations', 'number_populations', default=100, help='Number of populations')
 def create_generations(pop_size, number_generations, freq_A, number_populations):
 
     click.echo('Number of populations %d' % number_populations)
@@ -40,7 +40,7 @@ def create_generations(pop_size, number_generations, freq_A, number_populations)
 
     generations = []
     populations = []
-
+    pop_json = {}
 
     for n in range(1, number_populations + 1):
         frequency = []
@@ -63,36 +63,15 @@ def create_generations(pop_size, number_generations, freq_A, number_populations)
                     gene2 = 'a'
                 individuals.append(gene1 + gene2)
                 genes += gene1 + gene2
-            print(genes, genes.count('A'), genes.count('a'), genes.count('A')/(pop_size*2))
             frequency.append(genes.count('A')/(pop_size*2))
         populations.append(frequency)
 
-    print(populations)
+    for i, pop in enumerate(populations):
+        pop_json['pop_' + str(i)] = pop
 
-    # for n in range(1, number_generations + 1):
-    #     current_generation = []
-    #     for np in range(1, number_populations + 1):
-    #         current_population = []
-    #         genes = []
-    #         for p in range(pop_size):
-    #             gene1 = random.uniform(0, 1)
-    #             gene2 = random.uniform(0, 1)
-    #             # if gene1 >= frequency[n - 1]:
-    #             if gene1 >= freq_A:
-    #                 gene1 = 'A'
-    #             else:
-    #                 gene1 = 'a'
-    #             # if gene2 >= frequency[n - 1]:
-    #             if gene2 >= freq_A:
-    #                 gene2 = 'A'
-    #             else:
-    #                 gene2 = 'a'
-    #             individual = gene1 + gene2
-    #             current_population.append(individual)
-    #             calculate_frequency(current_population)
-    #     populations.append(current_population)
-    #
-    # print(len(populations))
+    drift_output = open('drift.json', 'w')
+    json.dump(pop_json, drift_output)
+    drift_output.close()
 
 
 if __name__ == '__main__':
