321561b61c5ec6b9e689813c1681df78a581479c
Paulo Nuin <nuin@genedrift.org>
2021-01-26 19:30:44 -0700

    added drift plot with Altair

--- src/drift.py
+++ src/drift.py
@@ -28,7 +28,7 @@ def calculate_frequency(population):
 @click.option('-p', '--pop-size', 'pop_size', default=100, help='Population size')
 @click.option('-f', '--freq-A', 'freq_A', default=0.5, help='Initial A frequency')
 @click.option('-n', '--n-generations', 'number_generations', default=100, help='Number of generations')
-@click.option('-np','--n-populations', 'number_populations', default=100, help='Number of populations')
+@click.option('-np','--n-populations', 'number_populations', default=10, help='Number of populations')
 def create_generations(pop_size, number_generations, freq_A, number_populations):
 
     click.echo('Number of populations %d' % number_populations)
@@ -41,7 +41,7 @@ def create_generations(pop_size, number_generations, freq_A, number_populations)
     generations = []
     populations = []
     pop_json = {}
-
+    scale = list(range(number_generations + 1))
     for n in range(1, number_populations + 1):
         frequency = []
         frequency.append(freq_A)
@@ -69,6 +69,7 @@ def create_generations(pop_size, number_generations, freq_A, number_populations)
     for i, pop in enumerate(populations):
         pop_json['pop_' + str(i)] = pop
 
+    # pop_json['scale'] = scale
     drift_output = open('drift.json', 'w')
     json.dump(pop_json, drift_output)
     drift_output.close()
